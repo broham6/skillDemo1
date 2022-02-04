8549d2c34d4e08c12781b09a1d8185690024551b
broham6 <73806352+broham6@users.noreply.github.com>
2022-02-04 15:22:16 -0800

    fixed

---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void multiplication(){
+        assertEquals(4, skillDemo.mult(2,2));
+    }
+}
